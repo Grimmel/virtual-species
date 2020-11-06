98ba82c807c4738dab9ef65389d74c5d2d2ce66c
liam <liamgrimmett47@gmail.com>
2020-11-06 22:46:20 +1100

    Updated comments
    Updated comments, LHS tests

--- cellular-automata/ca.jl
+++ cellular-automata/ca.jl
@@ -3,34 +3,60 @@ using Distributions
 using Random
 using StatsBase
 
-export colonise,coloniseMultiple,coloniseNeighbourWeight,extinction,extinctionNeighbour,OccurenceCellularAutomata
+export colonise,coloniseMultiple,coloniseNeighbourWeight,extinction,extinctionNeighbourWeight,OccurenceCellularAutomata
 
 
 struct OccurenceCellularAutomata
     pa::Matrix{Float64}
     pa_cart_index::Matrix{CartesianIndex{2}}
     suitability::Matrix{Float64}
+    # Dispersal parameters
+    meanDispersal::Float64
     dispersalProbability::Float64
     maxNumberDispersers::Int64
+    # Weight parameters
     neighbourSurvivalWeight::Float64
     neighbourDispersalWeight::Float64
-    meanDispersal::Float64
+    neighbourWeightMatrix::Matrix{Float64}
+    r::Int64
+
 end
 
+"""
+    newPos(meanDistance,cartIdx)
 
-function newPos(meanDistance,cartIdx)
+Calculates a new position that does not include a starting position (cartIdx).
+This is achived stochastically by sampling distance from an exponential
+distribution and direction of movement from a uniform distribution between 0-360.
+
+Note: The selected distance as a constant added to it in order to ensure a
+      distance less than 1 does not select a position the same as the starting one.
+      This value is +0.7071 because this is the ratio between the distance to
+      a direct:diagonal neighbour.
+      This ensures that rounding the X and Y values the direct neighbours are
+      selected more frequently than the diagonals. Higher values will bias to
+      diagonals and lower values will result in selection of the starting position.
+
+"""
+function newPos(meanDistance::Float64,startPos::CartesianIndex)
     distance = rand(Exponential(meanDistance),1)
-    # Distance needs to be a cell outside current cell.
-    # Distance from centre of current cell to adject, compated to diagonal
-    # is 1/2^-2 (approx 0.7071).Adding 1 would bias towards the diagonals.
+    # Add constant to ensure selection of pos outside current cell.
     distance = distance[1]+0.7071
     angle = 360.0*rand()
     # Remember 1 = y, 2 = x
-    x = Int(round(cos(deg2rad(angle))*distance,digits=0)) + cartIdx[2]
-    y = Int(round(sin(deg2rad(angle))*distance,digits=0)) + cartIdx[1]
+    x = Int(round(cos(deg2rad(angle))*distance,digits=0)) + startPos[2]
+    y = Int(round(sin(deg2rad(angle))*distance,digits=0)) + startPos[1]
     coord = (y,x)
     return(coord)
 end
+"""
+    selectProportion(pa,caIndex,dispersalProbability)
+
+Given a 2D array of occurence, randomly select cells that will disperse at
+the next time step. The number of dispersers is determined stochastically
+by sampling from a binomial distribution.
+
+"""
 function selectProportion(pa,caIndex,dispersalProbability)
     nPresences = Int(sum(pa))
     total = Array{CartesianIndex}(undef, nPresences)
@@ -42,18 +68,52 @@ function selectProportion(pa,caIndex,dispersalProbability)
         end
 
     end
-    numberDispersing = sample(total,rand(Binomial(nPresences,dispersalProbability)))
+    dispersers = sample(total,rand(Binomial(nPresences,dispersalProbability)))
+end
+"""
+    neighbourHoodWeight(ca)
+
+Select a single cell to colonise
+
+"""
+function neighbourHoodWeight(pa::Matrix{Float64},suit::Matrix{Float64},idx::CartesianIndex,shape::Tuple,weightMatrix::Matrix{Float64},r::Int64)
+    # Assumes square neighbourhood
+    ymin = idx[1]-r
+    ymax = idx[1]+r
+    xmin = idx[2]-r
+    xmax = idx[2]+r
+    idxYMin = 1
+    idxYMax = 1+2*r
+    idxXMin = 1
+    idxXMax = 1+2*r
+    # Check boundary conditions
+    if ymin < 1
+        ymin = 1
+        idxYMin = r+(2-idx[1])
+    end
+    if ymax > shape[1]
+        ymax = shape[1]
+        idxYMax = (r+1)+shape[1]-idx[1]
+    end
+    if xmin < 1
+         xmin = 1
+         idxXMin = r+(2-idx[2])
+      end
+    if xmax > shape[2]
+        xmax = shape[2]
+        idxXMax = (r+1)+shape[2]-idx[2]
+    end
+    oc = vec(pa[ymin:ymax,xmin:xmax])
+    su = vec(suit[ymin:ymax,xmin:xmax])
+    weight = oc.*su.*vec(weightMatrix[idxYMin:idxYMax,idxXMin:idxXMax])
+    #weight = oc.*vec(weightMatrix[idxYMin:idxYMax,idxXMin:idxXMax])
+    return(sum(weight))
 end
 """
     colonise(ca)
 
 Select a single cell to colonise
 
-# Examples
-```julia-repl
-julia> bar([1, 2], [1, 2])
-1
-```
 """
 function colonise(ca::OccurenceCellularAutomata)
     shape = size(ca.pa)
@@ -69,15 +129,11 @@ end
 """
     coloniseSuitWeight(ca)
 
-Select a number of dispersing cells and randomly selects how many cells each can
-colonise. A maximum number of dispersers is needed to be defined, with the
-actual number of dispersers weighted by suitability.
+Randomly selects a number cells that attempt to colonise nearby cells.
+A maximum number of dispersers is specified and each cell selected to disperse
+has
 
-# Examples
-```julia-repl
-julia> bar([1, 2], [1, 2])
-1
-```
+
 """
 function coloniseSuitWeight(ca::OccurenceCellularAutomata)
     shape = size(ca.pa)
@@ -103,85 +159,39 @@ end
 """
     coloniseNeighbourWeight(ca)
 
-For each occupied cell in the CA, determine the if that cell will disperse and
-use an exponential kernel to determine where it will colonise. Distance is
-sampled randomly from an exponential distribution, while direction is sampled
-from a uniform distribution between 0-360.
+Randomly selects a number cells that attempt to colonise nearby cells.
+Each cell selected to disperse has a random number of attempts at colonising
+selected from a Poisson distribution. For each attempt, the new position is
+selected randomly and converted to occupied (1.0).
 
-# Examples
-```julia-repl
-julia> bar([1, 2], [1, 2])
-1
-```
+Note: As this is neighbour weighted, the order of cells are randomised to prevent
+      any bias due to the iterating over cells in the same order each time the
+      function is called. This effectively makes the neighbourhood effects
+      asynchronus across the landscape.
+      An alternative would be to copy the occurence matrix and refer to this for
+      neighbourhood weights. This would effectively make the neighbourhood effects
+      synchronus (i.e. all processes happen at the same time, instantly).
 """
 function coloniseNeighbourWeight(ca::OccurenceCellularAutomata)
-    shape = size(ca.pa)
+    shp = size(ca.pa)
     dCells = selectProportion(ca.pa,ca.pa_cart_index,ca.dispersalProbability)
     # Random shuffle to avoid grid index bias due to order of applying this function
     idxShuffle = sample(collect(1:1:length(dCells)),length(dCells),replace=false)
     for i in idxShuffle
         cellIndex = dCells[i]
-        neighbourWeight = neighbourHoodWeight(ca.pa,ca.suitability,cellIndex,shape)
+        neighbourWeight = neighbourHoodWeight(ca.pa,ca.suitability,cellIndex,shp,ca.neighbourWeightMatrix,ca.r)
         dispWeight = neighbourWeight * ca.neighbourDispersalWeight
         # Determine maximum number of dispersers scaled by suitabiility
         dispersalMultiplier = 1/(1+MathConstants.e^(-10*(ca.suitability[cellIndex]-(1-dispWeight))))
-        maxNumDispersers = (ca.suitability[cellIndex] + (1-ca.suitability[cellIndex])*dispersalMultiplier)*ca.maxNumberDispersers
-        numberDispersers = rand(Poisson(maxNumDispersers))
+        meanNumDispersers = (ca.suitability[cellIndex] + (1-ca.suitability[cellIndex])*dispersalMultiplier)*ca.maxNumberDispersers
+        numberDispersers = rand(Poisson(meanNumDispersers))
         for j in 1:numberDispersers
             newXY = newPos(ca.meanDispersal,cellIndex)
-            if newXY[2]>=1 && newXY[2] <= shape[2] && newXY[1] >=1 && newXY[1]<=shape[1]
+            if newXY[2]>=1 && newXY[2] <= shp[2] && newXY[1] >=1 && newXY[1]<=shp[1]
                 ca.pa[newXY[1],newXY[2]] = 1.0
             end
         end
     end
-end
-function neighbourHoodWeight(pa,suit,idx,shape,weightMatrix,r)
-    # Assumes square neighbourhood
-    ymin = idx[1]-r
-    ymax = idx[1]+r
-    xmin = idx[2]-r
-    xmax = idx[2]+r
-    idxYMin = 1
-    idxYMax = 1+2*r
-    idxXMin = 1
-    idxXMax = 1+2*r
-    # Check boundary conditions
-    if ymin <= 0; ymin = 1,idxYMin = 2*r-idx[1] end
-    if ymax > shape[1]; ymax = shape[1],idxYMax = (r+1)+shape[1]-idx[1] end
-    if xmin <= 0; xmin = 1,idxXMin = 2*r-idx[2] end
-    if xmax > shape[2]; xmax = shape[2], idxYMax = (r+1)+shape[2]-idx[2] end
-    oc = vec(pa[ymin:ymax,xmin:xmax])
-    su = vec(suit[ymin:ymax,xmin:xmax])
-    weight = oc.*su.*vec(weightMatrix)
-    weight = weight - (pa[idx]*suit[idx]*weightMatrix[(r+1,r+1)])
-end
-function neighbourHoodWeight2(pa,suit,idx,shape)
-    # Define 3x3 neighbourhood boundaries
-    ymin = idx[1]-1
-    ymax = idx[1]+1
-    xmin = idx[2]-1
-    xmax = idx[2]+1
-    # Check boundary conditions
-    if ymin <= 0
-        ymin = 1
-    end
-    if ymax > shape[1]
-        ymax = shape[1]
-    end
-    if xmin <= 0
-        xmin = 1
-    end
-    if xmax > shape[2]
-        xmax = shape[2]
-    end
-    oc = pa[ymin:ymax,xmin:xmax]
-    su = suit[ymin:ymax,xmin:xmax]
-    pctColonised = 0
-    for i in eachindex(oc)
-        pctColonised = pctColonised+(oc[i]*su[i])
-    end
-    pctColonised = (pctColonised-(pa[idx]*suit[idx]))/8
-    return pctColonised
 end
 
 
@@ -195,7 +205,20 @@ function extinction(ca::OccurenceCellularAutomata)
         end
     end
 end
-function extinctionNeighbour(ca::OccurenceCellularAutomata)
+"""
+    extinctionNeighbourWeight(ca)
+
+Each occupied cell is
+
+Note: As this is neighbour weighted, the order of cells are randomised to prevent
+      any bias due to the iterating over cells in the same order each time the
+      function is called. This effectively makes the neighbourhood effects
+      asynchronus across the landscape.
+      An alternative would be to copy the occurence matrix and refer to this for
+      neighbourhood weights. This would effectively make the neighbourhood effects
+      synchronus (i.e. all processes happen at the same time, instantly).
+"""
+function extinctionNeighbourWeight(ca::OccurenceCellularAutomata)
     # Randomise cell index to prevent bias from the neighbourhood weighting
     # Alternative could be to copy the the pa array.
     shp = size(ca.pa)
@@ -205,7 +228,7 @@ function extinctionNeighbour(ca::OccurenceCellularAutomata)
         cellIndex = ca.pa_cart_index[i]
         if ca.pa[i] === 1.0
             # Determine the neighbourhood weighted survival probability
-            neighbourWeight = neighbourHoodWeight(ca.pa,ca.suitability,cellIndex,shp)
+            neighbourWeight = neighbourHoodWeight(ca.pa,ca.suitability,cellIndex,shp,ca.neighbourWeightMatrix,ca.r)
             survWeight = neighbourWeight * ca.neighbourSurvivalWeight
             # Logistic function to scale survival proba
             sf = 1/(1+MathConstants.e^(-10*(ca.suitability[cellIndex]-(1-survWeight))))
@@ -217,10 +240,6 @@ function extinctionNeighbour(ca::OccurenceCellularAutomata)
             end
         end
     end
-end
-
-function rescale(x,newMin,newMax)
-    return (newMax-newMin).*((x.-minimum(x))./(maximum(x)-minimum(x))).+newMin
 end
 
 end # Module
--- cellular-automata/simulations.jl
+++ cellular-automata/simulations.jl
@@ -1,6 +1,7 @@
 include("ca.jl")
 using .CellularAutomata
 using DelimitedFiles
+using DataFrames
 
 function runSimulations()
     # Parameters
@@ -143,5 +144,160 @@ function testParams2()
         end
     end
 end
+function testParams3()
+    survWeight = [0.1,0.5,1,3]
+    nbR = [1,2,3,4]
+    suit = readdlm("D:/PHDExperimentOutputs/Transferability/landscapes/suitability/suitability232.asc",skipstart=6)
+    suit = rescale(suit,0,0.75,0,1)
+    for sweight in survWeight
+        for r in nbR
+            m = idwDistWeightMatrix(4,1)
+            weightMatrix = idwDistWeightMatrix(r,10)
+            for i in 1:10
+                simName = "ns10sim_"*string(sweight)*"_"*string(r)*"_rep"*string(i)
+                pa = ones(400,400)
+                paIdx = CartesianIndices(pa)
+                ca = OccurenceCellularAutomata(pa,paIdx,suit,2.0,0.4,3,sweight,1.0,weightMatrix,r)
+                for j in 1:100
+                    coloniseNeighbourWeight(ca)
+                    extinctionNeighbourWeight(ca)
+                end
+                open("D:/PHDExperimentOutputs/Transferability/ca_tuning/r6/"*simName*".asc","w") do io
+                    write(io,"NCOLS 400\nNROWS 400\nXLLCORNER 0\nYLLCORNER 0\nCELLSIZE 100\nNODATA_value -9999\n")
+                    writedlm(io,pa)
+                end
+            end
+        end
+    end
+end
+function testParams4()
+    survWeight = [0.01,0.05]
+    nbR = [1,2,3]
+    scaleFactor = [0.6,0.75,"na"]
+    landscapes = [232,238,218,188]
+    for ls in landscapes
+        for sf in scaleFactor
+            suit = readdlm("D:/PHDExperimentOutputs/Transferability/landscapes/suitability/suitability"*string(ls)*".asc",skipstart=6)
+            if sf != "na"
+                suit = rescale(suit,0,sf,0,1)
+            end
+            for sweight in survWeight
+                for r in nbR
+                    weightMatrix = idwDistWeightMatrix(r)
+                    for i in 1:10
+                        simName = "ls_"*string(ls)*"sim_"*string(sweight)*"_"*string(sf)*"_"*string(r)*"_rep"*string(i)
+                        pa = ones(400,400)
+                        paIdx = CartesianIndices(pa)
+                        ca = OccurenceCellularAutomata(pa,paIdx,suit,2,0.3,4,sweight,1.0,weightMatrix,r)
+                        for j in 1:100
+                            coloniseNeighbourWeight(ca)
+                            extinctionNeighbourWeight(ca)
+                        end
+                        open("D:/PHDExperimentOutputs/Transferability/ca_tuning/r4/"*simName*".asc","w") do io
+                            write(io,"NCOLS 400\nNROWS 400\nXLLCORNER 0\nYLLCORNER 0\nCELLSIZE 100\nNODATA_value -9999\n")
+                            writedlm(io,pa)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+function testParamsDisp()
+    distance = [1,3,6]
+    nDisperser = [1,2,5]
+    nbWeight = [0.01,0.5,1.0]
+    prob = [0.1,0.5,0.8]
+    suit = readdlm("D:/PHDExperimentOutputs/Transferability/landscapes/suitability/suitability232.asc",skipstart=6)
+    suit = rescale(suit,0,0.75,0,1)
+    for d in distance
+        for ndisp in nDisperser
+            for nb in nbWeight
+                for p in prob
+                    weightMatrix = idwDistWeightMatrix(1)
+                    for i in 1:10
+                        simName = "sim_"*string(d)*"_"*string(ndisp)*"_"*string(p)*"_"*string(nb)*"_rep"*string(i)
+                        pa = ones(400,400)
+                        paIdx = CartesianIndices(pa)
+                        ca = OccurenceCellularAutomata(pa,paIdx,suit,d,p,ndisp,0.5,nb,weightMatrix,1)
+                        for j in 1:100
+                            coloniseNeighbourWeight(ca)
+                            extinctionNeighbourWeight(ca)
+                        end
+                        open("D:/PHDExperimentOutputs/Transferability/ca_tuning/r5/"*simName*".asc","w") do io
+                            write(io,"NCOLS 400\nNROWS 400\nXLLCORNER 0\nYLLCORNER 0\nCELLSIZE 100\nNODATA_value -9999\n")
+                            writedlm(io,pa)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+function test()
+    distance = [1,3,6]
+    nDisperser = [1,2,5]
+    nbWeight = [0.01,0.5,1.0]
+    prob = [0.1,0.5,0.8]
+    suit = readdlm("D:/PHDExperimentOutputs/Transferability/landscapes/suitability/suitability232.asc",skipstart=6)
+    suit = rescale(suit,0,0.75,0,1)
+    weightMatrix = idwDistWeightMatrix(1)
+    pa = ones(400,400)
+    paIdx = CartesianIndices(pa)
+    ca = OccurenceCellularAutomata(pa,paIdx,suit,2,0.3,3,0.5,0.1,weightMatrix,1)
+    for j in 1:100
+        coloniseNeighbourWeight(ca)
+        extinctionNeighbourWeight(ca)
+    end
+    # open("D:/PHDExperimentOutputs/Transferability/ca_tuning/r5/"*simName*".asc","w") do io
+    #     write(io,"NCOLS 400\nNROWS 400\nXLLCORNER 0\nYLLCORNER 0\nCELLSIZE 100\nNODATA_value -9999\n")
+    #     writedlm(io,pa)
+    # end
+end
+function testLHS()
+    lhsParamSet = readdlm("D:/PHDExperimentOutputs/Transferability/ca_tuning/LHS_1.csv", ',', Float64,skipstart=1)
+    for paramSet in lhsParamSet
+        print(paramSet)
+        param_sample_id = paramSet[1]
+        mean_dispersal = paramSet[2]
+        dispersal_prob = paramSet[3]
+        disp_nb_weight = paramSet[4]
+        surv_nb_weight = paramSet[5]
+        suit_max = paramSet[6]
+        nb_radius = Int(paramSet[7])
+        println(nb_radius)
+        nb_weight_mat = idwDistWeightMatrix(nb_radius,10)
+        max_dispersers = paramSet[8]
 
-testParams2()
+        suit = readdlm("D:/PHDExperimentOutputs/Transferability/landscapes/suitability/suitability232.asc",skipstart=6)
+        suit = rescale(suit,0,suit_max,0,1)
+        for i in 1:10
+            simName = "sim_"*string(param_sample_id)*"_ls232_rep"*string(i)
+            pa = ones(400,400)
+            paIdx = CartesianIndices(pa)
+            ca = OccurenceCellularAutomata(pa,paIdx,suit,mean_dispersal,dispersal_prob,max_dispersers,surv_nb_weight,disp_nb_weight,nb_weight_mat,nb_radius)
+            for j in 1:100
+                coloniseNeighbourWeight(ca)
+                extinctionNeighbourWeight(ca)
+            end
+            open("D:/PHDExperimentOutputs/Transferability/ca_tuning/lhs1/"*simName*".asc","w") do io
+                write(io,"NCOLS 400\nNROWS 400\nXLLCORNER 0\nYLLCORNER 0\nCELLSIZE 100\nNODATA_value -9999\n")
+                writedlm(io,pa)
+            end
+        end
+    end
+end
+
+function idwDistWeightMatrix(r,scale)
+    w = 2*r+1
+    m = ones(w,w)
+    mIdx = CartesianIndices(m)
+    for idx in mIdx
+        #print(idx[1])
+        m[idx] = sqrt(((idx[1]-(r+1))^2)+((idx[2]-(r+1))^2))
+    end
+    m = (m.^-1)/scale
+    m[r+1,r+1] = 0.0
+    return(m)
+end
+testLHS()
